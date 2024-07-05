1de06ac17fe165b19183212f8aaf0ade368679e3
tawatson02 <ta_watson@live.com>
2024-07-05 11:31:04 -0500

    Implemented viewAllDepartments, viewAllRoles, and viewAllEmployees functions. Each function outputs the result in a table format and then returns to the main menu when completed.

--- index.js
+++ index.js
@@ -44,4 +44,37 @@ const mainMenu = () => {
                 process.exit();
         }
     });
+}; 
+
+const viewAllDepartments = () => {
+    db.query('SELECT * FROM departments', (err, res) => {
+        if (err) throw err;
+        console.table(res.rows);
+        mainMenu();
+    });
+};
+
+const viewAllRoles = () => {
+    const query = `SELECT roles.id, roles.title, roles.salary, departments.name AS department
+    FROM roles
+    LEFT JOIN departments ON roles.department_id = departments.id`;
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res.rows);
+        mainMenu();
+    });
+};
+
+const viewAllEmployees = () => {
+    const query = `SELECT employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary,
+    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employees
+    LEFT JOIN roles ON employees.role_id = roles.id
+    LEFT JOIN departments on roles.department_id = departments.id
+    LEFT JOIN employees manager ON manager.id = employee.manager_id`;
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res.rows);
+        mainMenu();
+    });
 };
